1f45f64316a524a7009bc34948755fe5f8e60653
tocanpolina <tocanpolina@gmail.com>
2023-12-02 11:00:14 +0200

    db ciocolate

---
+++ script.js
@@ -0,0 +1,24 @@
+let productGrid = document.getElementById('products-grid')
+let productsArray = [];
+let xhr = new XMLHttpRequest();
+let url = "https://my-json-server.typicode.com/RobocodeSchool/marketplace"
+
+xhr.open('GET',url +'/products');
+xhr.responseType = 'json';
+xhr.onload = function(){
+	let products = xhr.response;
+	productGrid.innerHTML = null;
+	products.forEach(p=>{
+		productsArray.push(p);
+		let pElem = document.createElement('div');
+		pElem.classList.add('product');
+		pElem.innerHTML = `
+			<h2 class='product-name'>${p.name}</h2>
+			<img class='product-photo' src='${p.photo_url}''>
+			<p class='product-description'><b>Description</b>${p.description}</p>
+			<button>Buy</button>`;
+			productGrid.append(pElem);
+	});
+}
+
+xhr.send();
